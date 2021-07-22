bae7562c882360d30fab9d6dd98f3a20a8933a71
GuiAndrew <guiandrew.pt@outlook.com>
2021-07-22 17:58:48 +0100

    variables

---
+++ Variables/variables.playground/Contents.swift
@@ -0,0 +1,19 @@
+// Variables
+
+// Declare a variable
+// Variables with key word var, can be changed(mutable) once initialized
+var name: String
+name = "Klaus"
+name = "Klaus M."
+
+// Variables with key word let, can't be changed(imutable) once initialized
+// This is call a constant
+let age: Int
+age = 1000
+// age = 1001 // Error -> mmutable value 'age' may only be initialized once
+
+var city: String = "New Orleans" // Explicit typed; Or type annotation;
+var continent = "U.S.A" // String; Type inference
+
+var sumOne: Int = 1 // Integer
+var height = 1.81 // Double
